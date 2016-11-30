c8d17eacbb9b1c449d00f857167d9421e975820f
Sebulbion <sebbetengdahl@hotmail.com>
2016-12-01 09:10:43 +1300

    Did ScaleVector v1.0

--- GeometryProject/geometry.cpp
+++ GeometryProject/geometry.cpp
@@ -26,3 +26,13 @@ TVector3& Subtract(const TVector3& _krA,
 	_rResultant.m_fZ = _krA.m_fZ - _krB.m_fZ;
 	return _rResultant;
 }
+
+TVector3& ScaleVector(const TVector3& _krA,
+	const float _kfScalar,
+	TVector3& _rResultant)
+{
+	_rResultant.m_fX = _krA.m_fX * _kfScalar;
+	_rResultant.m_fY = _krA.m_fY * _kfScalar;
+	_rResultant.m_fZ = _krA.m_fZ * _kfScalar;
+	return _rResultant;
+}
